7d44d8c6df40492665cc3ffad95d07ec6bc68926
Blitzsnchell <134215409+Blitzsnchell@users.noreply.github.com>
2024-07-09 23:04:02 +0700

    New and improve Homepage(Recipe and profile is under develop)

--- app/src/main/java/com/example/prm_project/HomeActivity.java
+++ app/src/main/java/com/example/prm_project/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -14,53 +15,69 @@ import com.google.android.material.tabs.TabLayout;
 
 
 public class HomeActivity extends AppCompatActivity {
-
+    ImageView home_button;
+    ImageView addFood_button;
+    ImageView caculateBmi_button;
+    ImageView history_button;
+    ImageView recipe_button;
+    ImageView profile_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        // Set the text of the welcome message
-        TextView welcomeMessage = findViewById(R.id.welcome_message);
-        welcomeMessage.setText("Welcome, User");
-
-        // Set the date of the calendar
-        TextView calendarDate = findViewById(R.id.calendar_date);
-        calendarDate.setText("Monday, 18 June 2022");
-
-        // Set the calorie count
-        TextView calorieCount = findViewById(R.id.calorie_count);
-        calorieCount.setText("619 cal");
-
-        // Set the images for each meal
-
-
-        // Set the click listeners for each button
-        Button calculateButton = findViewById(R.id.calculateBmi_button);
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        //home button
+        home_button = (ImageView) findViewById(R.id.home_button);
+        home_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,BmiActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this,HomeActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
-
-        Button historyButton = findViewById(R.id.history_button);
-        historyButton.setOnClickListener(new View.OnClickListener() {
+        //addFood Button
+        addFood_button = (ImageView) findViewById(R.id.addFood_button);
+        addFood_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,HistoryActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button addButton = findViewById(R.id.addFood_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this,CaloriesAcitivity.class);
                 startActivity(intent);
             }
         });
-    }
-}
+        //caculateBMI
+        caculateBmi_button =(ImageView) findViewById(R.id.calculateBmi_button);
+        caculateBmi_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(HomeActivity.this,BmiActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        //historyButton
+            history_button = (ImageView) findViewById(R.id.history_button);
+            history_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent =new Intent(HomeActivity.this,HistoryActivity.class);
+                    startActivity(intent);
+                }
+            });
+        //RecipeButton(under develop)
+            recipe_button = (ImageView) findViewById(R.id.recipe_button);
+            recipe_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //still under develope
+                }
+            });
+        //ProfileButton(under develope)
+            profile_button = (ImageView) findViewById(R.id.profile_button);
+            profile_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //still under develope
+                }
+            });
+            }
+        }
